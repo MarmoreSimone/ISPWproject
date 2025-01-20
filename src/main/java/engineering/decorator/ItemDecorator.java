629a8a13e3dee8d03b3b0446514a63a0cf1b1cad
marmoresimone <marmoresimone14@gmail.com>
2025-01-20 01:59:37 +0100

    decorator

--- src/main/java/engineering/decorator/ItemDecorator.java
+++ src/main/java/engineering/decorator/ItemDecorator.java
@@ -6,45 +6,39 @@ public abstract class ItemDecorator extends MenuItem{
     //aggregazione
     private MenuItem item;
 
-    public ItemDecorator(MenuItem item){
+    protected ItemDecorator(MenuItem item){
         this.item = item;
     }
 
 
     @Override
     public String getName(){
-        String resultsFromRedirection = this.item.getName();
-        return resultsFromRedirection;
+        return this.item.getName();
     }
 
     @Override
     public double getPrice(){
-        Double resultsFromRedirection = this.item.getPrice();
-        return resultsFromRedirection;
+        return this.item.getPrice();
     }
 
     @Override
     public double getCalories(){
-        Double resultsFromRedirection = this.item.getCalories();
-        return resultsFromRedirection;
+        return this.item.getCalories();
     }
 
     @Override
     public double getCaffeine(){
-        Double resultsFromRedirection = this.item.getCaffeine();
-        return resultsFromRedirection;
+        return this.item.getCaffeine();
     }
 
     @Override
     public String getDescription(){
-        String resultsFromRedirection = this.item.getDescription();
-        return resultsFromRedirection;
+        return this.item.getDescription();
     }
 
     @Override
     public String getImage(){
-        String resultsFromRedirection = this.item.getImage();
-        return resultsFromRedirection;
+        return this.item.getImage();
     }
 
 
